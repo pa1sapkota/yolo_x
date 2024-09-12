04645326d34c8d59d2f3320a24497d409b0e6931
pa1sapkota <pawan@fusemachines.com>
2024-09-12 20:45:58 +0545

    default config

--- run.py
+++ run.py
@@ -13,24 +13,12 @@ class Exp(MyExp):
         self.depth = 1.33
         self.width = 1.25
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-        self.warmup_epochs = 5
-        self.max_epoch = 300
-        self.warmup_lr = 0
-        self.basic_lr_per_img = 0.01 / 64.0
-        self.scheduler = "yoloxwarmcos"
-        self.no_aug_epochs = 15
-        self.min_lr_ratio = 0.05
-        self.ema = True
+        self.max_epoch = 300 # change 
 
-        self.weight_decay = 5e-4
-        self.momentum = 0.9
+
         # Define yourself dataset path
         self.data_dir = "datasets/coco128" 
         self.train_ann = "instances_train2017.json"
         self.val_ann = "instances_val2017.json"
 
         self.num_classes = 11
-
-        # self.max_epoch = 300
-        # self.data_num_workers = 4
-        # self.eval_interval = 1
